0b9e3f2cd9c06a97d808adba12d375613fec5006
satoshi-murakumo <satoshi.murakumo@gmail.com>
2013-02-04 17:51:40 +0900

    fix up.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,66 +14,90 @@ import java.net.URLEncoder;
 
 public class Application extends Controller {
 
-  public static Result index() {
-    String query = "";
-    if (request().body().asFormUrlEncoded() != null) {
-      query = request().body().asFormUrlEncoded().get("_nkw")[0];
+    private static String getParam(Http.Request request, String key, String def) {
+        String[] values = request.queryString().get(key);
+        if (values != null) {
+            return values[0];
+        } else {
+            return def;
+        }
     }
 
-    if(null != query && query.length() > 0) {
-      String encoded;
-      try {
-        encoded = URLEncoder.encode(query, "UTF-8");
-      }
-      catch(UnsupportedEncodingException uee) {
-        Logger.warn(uee.getLocalizedMessage(), uee);
-        return ok(index.render(null));
-      }
+    public static Result index() {
+        String query = "";
+        if (request().queryString() != null) {
+            query = getParam(request(), "_nkw", "");
+        }
 
-      final String mode = request().body().asFormUrlEncoded().get("_mode")[0];
-      final String skipRender = request().body().asFormUrlEncoded().get("_skiprender")[0];
-      if("async".equals(mode)) {
-        return doAsync(encoded, "true".equals(skipRender));
-      }
-      else {
-        return doSync(encoded, "true".equals(skipRender));
-      }
+        if(null != query && query.length() > 0) {
+            String encoded;
+            try {
+                encoded = URLEncoder.encode(query, "UTF-8");
+            }
+            catch(UnsupportedEncodingException uee) {
+                Logger.warn(uee.getLocalizedMessage(), uee);
+                return ok(index.render(null, null));
+            }
+
+            final String mode = getParam(request() ,"_mode", "");
+            final String skipRender = getParam(request(), "_skiprender", "");
+            if("async".equals(mode)) {
+                return doAsync(query, encoded, "true".equals(skipRender));
+            }
+            else {
+                return doSync(query, encoded, "true".equals(skipRender));
+            }
+        }
+
+        return ok(index.render(null, null));
     }
 
-    return ok(index.render(null));
-  }
-
-  private static Result doSync(final String query, final boolean skipRender) {
-    final String url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&" +
-            "SERVICE-VERSION=1.8.0&SECURITY-APPNAME=Foobat96e-1d23-4ae7-8e15-e5874c9cd58&" +
-            "RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&" +
-            "keywords=" + query + "&paginationInput.entriesPerPage=50&" +
-            "outputSelector%280%29=SellerInfo&outputSelector%281%29=CategoryHistogram";
-    final JsonNode results = WS.url(url).get().get().asJson();
-    if(!skipRender) {
-      return ok(index.render(results));
+    private static Result doSync(final String query, final String encoded, final boolean skipRender) {
+        final WS.Response wsresponse = WS.url("http://svcs.ebay.com/services/search/FindingService/v1")
+            .setQueryParameter("OPERATION-NAME", "findItemsByKeywords")
+            .setQueryParameter("SERVICE-VERSION", "1.8.0")
+            .setQueryParameter("SECURITY-APPNAME", "Foobat96e-1d23-4ae7-8e15-e5874c9cd58")
+            .setQueryParameter("RESPONSE-DATA-FORMAT", "JSON")
+            .setQueryParameter("REST-PAYLOAD", "")
+            .setQueryParameter("keywords", encoded)
+            .setQueryParameter("paginationInput.entriesPerPage", "50")
+            .setQueryParameter("outputSelector(0)", "SellerInfo")
+            .setQueryParameter("outputSelector(1)", "CategoryHistogram")
+            .get().get();
+        final JsonNode results = wsresponse.asJson();
+        if(!skipRender) {
+            return ok(index.render(query, results));
+        }
+        else {
+            return ok();
+        }
     }
-    else {
-      return ok();
-    }
-  }
 
-  private static Result doAsync(final String encodedQuery, boolean skipRender) {
-    final String url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&" +
-            "SERVICE-VERSION=1.8.0&SECURITY-APPNAME=Foobat96e-1d23-4ae7-8e15-e5874c9cd58&" +
-            "RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&" +
-            "keywords=" + encodedQuery + "&paginationInput.entriesPerPage=50&" +
-            "outputSelector%280%29=SellerInfo&outputSelector%281%29=CategoryHistogram";
+    private static Result doAsync(final String query, final String encoded, final boolean skipRender) {
+        final WS.WSRequestHolder request = WS.url("http://svcs.ebay.com/services/search/FindingService/v1")
+            .setQueryParameter("OPERATION-NAME", "findItemsByKeywords")
+            .setQueryParameter("SERVICE-VERSION", "1.8.0")
+            .setQueryParameter("SECURITY-APPNAME", "Foobat96e-1d23-4ae7-8e15-e5874c9cd58")
+            .setQueryParameter("RESPONSE-DATA-FORMAT", "JSON")
+            .setQueryParameter("REST-PAYLOAD", "")
+            .setQueryParameter("keywords", encoded)
+            .setQueryParameter("paginationInput.entriesPerPage", "50")
+            .setQueryParameter("outputSelector(0)", "SellerInfo")
+            .setQueryParameter("outputSelector(1)", "CategoryHistogram");
 
-    return async(
-            WS.url(url).get().map(
+        return async(
+                request.get().map(
                     new F.Function<WS.Response, Result>() {
-                      public Result apply(WS.Response response) {
-                        return ok(index.render(response.asJson()));
-                      }
+                        public Result apply(WS.Response response) {
+                            if (!skipRender) {
+                                return ok(index.render(query, response.asJson()));
+                            } else {
+                                return ok();
+                            }
+                        }
                     }
-            )
-    );
-  }
-  
+                )
+               );
+    }
+
 }
